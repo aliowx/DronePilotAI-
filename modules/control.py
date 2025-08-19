39e21c9549ce39041fa0c9ef5dd34021a1efbdc4
Alexander <alialibakhshi9898@gmail.com>
2025-08-19 08:06:48 +0100

    feat:🎉 make Class to imporve the hight quality

--- modules/control.py
+++ modules/control.py
@@ -4,65 +4,31 @@ from simple_pid import PID
 
 logger = logging.getLogger(__name__) 
 
-USE_PID_YAW = True
-USE_PID_ROLL = False
-
-
-MAX_SPEED = 4
-MAX_YAW = 15
-
-
-P_YAW = 0.02
-I_YAW = 0
-D_YAW = 0
-
-
-P_ROLL = 0.22
-I_ROLL = 0
-D_ROLL = 0
-
-control_loop_active = True
-pidYaw = None
-pidRoll = None
-movementYawAngle = 0
-movementRollAngle = 0
-inputValueYaw = 0
-inputValueVelocityX = 0
-control_loop_active = True 
-flight_altitude = 4
-
-debug_yaw = None
-debug_velocity = None
 
 
 
-def configure_PID(control):
-   
-    global pidRoll, pidYaw
+class DroneController:
+    def __init__(
+        self,
+        use_pid_yaw=True,
+        use_pid_roll=False,
+        max_speed=4,
+        max_yaw=15,
+        flight_altitude=4,
+        control_freq=20
+    ):pass
+
+    """
+        High-level drone controller using PID control for yaw and roll.
+
+    Args:
+        use_pid_yaw (bool): Enable PID for yaw control.
+        use_pid_roll (bool): Enable PID for roll control.
+        max_speed (float): Max roll speed (m/s).
+        max_yaw (float): Max yaw rate (deg/s).
+        flight_altitude (float): Target altitude (meters).
+        control_freq (float): Control loop frequency (Hz).
+    """
     
-    """Creates PID """
     
-    logger.info('Configuring control')
     
-    if control == "PID":
-        pidYaw = PID(P_YAW, I_YAW, D_YAW, setpoint=0)       # I = 0.001
-        pidYaw.output_limits = (-MAX_YAW, MAX_YAW)          # PID Range
-        pidRoll = PID(P_ROLL, I_ROLL, D_ROLL, setpoint=0)   # I = 0.001
-        pidRoll.output_limits = (-MAX_SPEED, MAX_SPEED)     # PID Range
-        
-        logger.info('Configuring PID')
-        
-    else:
-        pidYaw = PID(P_YAW, 0, 0, setpoint=0)               # I = 0.001
-        pidYaw.output_limits = (-MAX_YAW, MAX_YAW)          # PID Range
-        pidRoll = PID(P_ROLL, 0, 0, setpoint=0)             # I = 0.001
-        pidRoll.output_limits = (-MAX_SPEED, MAX_SPEED)     # PID Range
-        
-        logger.info('Configuring PID')
-        
-        
-def connect_drone(drone_location):...
-
-
-
-
