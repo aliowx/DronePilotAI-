8bb56fe1d165e43be7cb6b1f38838ec02fef5816
Alexander <alialibakhshi9898@gmail.com>
2025-09-12 08:59:01 +0100

    feat:✅ fix

--- modules/vision.py
+++ modules/vision.py
@@ -14,3 +14,16 @@ def get_center(contour)->None:
 
 def euclidean_distance(p1, p2):
     return math.hypot(p1[0] - p2[0], p1[1]-p2[1])
+
+
+def axis_delta(v1, v2):
+    return v2 - v1
+
+
+
+def point_in_rectangle(point, left, right, top, bottom):
+    x, y = point
+    return left < x < right and top < y < bottom
+
+
+ 
