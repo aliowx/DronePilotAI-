4547e6e37242fdc851fef68b002c7ca8d749795c
Alexander <alialibakhshi9898@gmail.com>
2025-11-26 15:17:07 +0000

    feat:🧱 improve the class ZED

--- modules/zed.py
+++ modules/zed.py
@@ -5,10 +5,14 @@ import time
 
 
 class ZEDWrapper:
-    def __init__(self,performance_mode: bool = True, min_range: float = 1.0, max_range: float = 6.0) -> None:
+    def __init__(self, performance_mode: bool = True, min_range: float = 1.0, max_range: float = 6.0) -> None:
+        
+        self.performance_mode = performance_mode
+        self.min_range = min_range
+        self.max_range = max_range
         self.depth_camera = sl.Camera()
         self.depth_mat = sl.Mat()
         self.rgb_mat = sl.Mat()
         
         
-    
+        
